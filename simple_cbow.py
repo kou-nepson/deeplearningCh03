6479bab7656672466a667abb714880dec9fabfda
杉山航 <cjgjc803@gmail.com>
2018-11-16 17:34:33 +0900

    CBOWモデルの実装

---
+++ simple_cbow.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from layers import MatMul, SoftmaxWithLoss
+class SimpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        self.in_layer0 = Matmul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        self.word_vecs = W_in
